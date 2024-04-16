fb630e6bfd65658b2abdea47c1c12eed5e414a34
yunzhaol <107370489+yunzhaol@users.noreply.github.com>
2024-04-16 19:26:13 -0400

    Update 02-data_cleaning.R

--- scripts/02-data_cleaning.R
+++ scripts/02-data_cleaning.R
@@ -1,44 +1,30 @@
 #### Preamble ####
-# Purpose: Cleans the raw plane data recorded by two observers..... [...UPDATE THIS...]
-# Author: Yunzhao Li [...UPDATE THIS...]
-# Date: 26 March 2024 [...UPDATE THIS...]
-# Contact: yunzhao.li@mail.utoronto.ca [...UPDATE THIS...]
+# Purpose: Cleans the raw plane data recorded by two observers..... 
+# Author: Yunzhao Li
+# Date: 26 March 2024
+# Contact: yunzhao.li@mail.utoronto.ca
 # License: MIT
-# Pre-requisites: [...UPDATE THIS...]
-# Any other information needed? [...UPDATE THIS...]
+# Pre-requisites: None
 
 #### Workspace setup ####
+install.packages("tidyverse")
 library(tidyverse)
 
 #### Clean data ####
-raw_data <- read_csv("inputs/data/plane_data.csv")
+aerial_priority <- read_csv("data/raw_data/aerial_priority.csv")
 
-cleaned_data <-
-  raw_data |>
-  janitor::clean_names() |>
-  select(wing_width_mm, wing_length_mm, flying_time_sec_first_timer) |>
-  filter(wing_width_mm != "caw") |>
-  mutate(
-    flying_time_sec_first_timer = if_else(flying_time_sec_first_timer == "1,35",
-                                   "1.35",
-                                   flying_time_sec_first_timer)
+cleaned_aerial_priority <-
+  clean_names(aerial_priority) |>
+  filter(
+    tgt_country == "germany", !is.na(tgt_priority_explanation)
   ) |>
-  mutate(wing_width_mm = if_else(wing_width_mm == "490",
-                                 "49",
-                                 wing_width_mm)) |>
-  mutate(wing_width_mm = if_else(wing_width_mm == "6",
-                                 "60",
-                                 wing_width_mm)) |>
-  mutate(
-    wing_width_mm = as.numeric(wing_width_mm),
-    wing_length_mm = as.numeric(wing_length_mm),
-    flying_time_sec_first_timer = as.numeric(flying_time_sec_first_timer)
-  ) |>
-  rename(flying_time = flying_time_sec_first_timer,
-         width = wing_width_mm,
-         length = wing_length_mm
-         ) |> 
-  tidyr::drop_na()
+  select(tgt_priority_explanation, tgt_type, tgt_industry, country_flying_mission, msn_type, total_tons, ac_attacking
+)
+
+head(cleaned_aerial_priority)
+
+
 
 #### Save data ####
 write_csv(cleaned_data, "outputs/data/analysis_data.csv")
+
