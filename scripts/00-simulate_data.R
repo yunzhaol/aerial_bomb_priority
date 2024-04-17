4a7846c9465e5e245a2d0fc5838c1e3274e2215a
Yunzhao Li <yunzhao.li@mail.utoronto.ca>
2024-04-17 16:07:43 +0000

    complete simlulation

--- scripts/00-simulate_data.R
+++ scripts/00-simulate_data.R
@@ -1,24 +1,29 @@
 #### Preamble ####
-# Purpose: Simulates an analysis dataset about Pokemon level and weakness against fire
+# Purpose: Simulates an analysis dataset about the aerial bombing campaigns against Germany
 # Author: Yunzhao Li
-# Date: 26 March 2024
+# Date: 16 April 2024
 # Contact: yunzhao.li@mail.utoronto.ca
 # License: MIT
-# Pre-requisites: -
-# Any other information needed? -
+# Pre-requisites: None
+
 
 
 #### Workspace setup ####
 library(tidyverse)
+library(janitor)
 
 #### Simulate data ####
-data <- 
-  tibble(
-    level = rnorm(n = 1000, mean = 100, sd = 10) |> floor(),
-    weakness = sample(x = c("fire", "not fire"), size = 1000, replace = TRUE)
+set.seed(23)
+
+simulated_aerial_data <- tibble(
+    tgt_priority_explanation = factor(sample(c('target of last resort', 'target of opportunity', 'primary target'), 1000, replace = TRUE), levels = c('target of last resort', 'target of opportunity', 'primary target')),
+    tgt_industry = sample(c('unidentified targets', 'synthetic oil refineries'), 1000, replace = TRUE),
+    country_flying_mission = sample(c('usa', 'great britain','others'), 1000, replace = TRUE),
+    total_tons = sample(1:40, 1000, replace = TRUE),
+    ac_attacking = sample(1:20, 1000, replace = TRUE)
   )
 
-
+head(simulated_aerial_data)
 
 
 
